2fd5b609948bbac741f8d65cfc95be7ea8090302
TomG <tomgosling57@gmail.com>
2025-09-27 14:15:20 +1000

    Implemented database connection with basic users model and initialize the database in the app file

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
+from database import init_db, create_initial_owner
+from routes.users import user_bp
+import secrets
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_bytes(32)
