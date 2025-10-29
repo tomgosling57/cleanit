0eae69da7349e226a0da605233c94e7ef42de73b
TomG <tomgosling57@gmail.com>
2025-10-29 15:07:15 +1100

    feat(teams bp): added get_teams & get_team & teardown_job_db methods

--- routes/teams.py
+++ routes/teams.py
@@ -7,5 +7,15 @@ from controllers import teams_controller
 teams_bp = Blueprint('teams', __name__, url_prefix='/teams')
 
 @teams_bp.teardown_request
-def teardown_request(exception=None):
-    teams_controller.teardown_db(exception)
+def teardown_job_db(exception=None):
+    teardown_db(exception)
+
+@teams_bp.route('/', methods=['GET'])
+@login_required
+def get_teams():
+    return teams_controller.get_teams()
+
+@teams_bp.route('/<int:team_id>', methods=['GET'])
+@login_required
+def get_team(team_id):
+    return teams_controller.get_team(team_id)
