70d2131175e38b52b2f22738c5f012e892d2c873
TomG <tomgosling57@gmail.com>
2025-10-29 14:24:47 +1100

    fix(team service): Updated ad and remove member methods to use the new team to user relationship

--- services/team_service.py
+++ services/team_service.py
@@ -7,21 +7,21 @@ class TeamService:
         self.db_session = db_session
 
     def get_team_members(self, team_id):
-        team = self.db_session.query(Team).options(joinedload(Team.members)).filter_by(id=team_id).first()
+        team = self.db_session.query(Team).options(joinedload(Team.members)).filter(Team.id == team_id).first()
         return team.members if team else []
 
-    def add_member_to_team(self, team_id, user_id):
-        team_member = TeamMembers(team_id=team_id, user_id=user_id)
-        self.db_session.add(team_member)
+    def add_member_to_team(self, team, user):
+        user.team_id = team.id
+        team.members.append(user)
         self.db_session.commit()
-        return team_member
 
-    def remove_member_from_team(self, team_id, user_id):
-        team_member = self.db_session.query(TeamMembers).filter_by(team_id=team_id, user_id=user_id).first()
-        if team_member:
-            self.db_session.delete(team_member)
+        return user
+
+    def remove_member_from_team(self, team, user):
+        if user in team.members:
+            team.members.remove(user)
+            user.team_id = None
             self.db_session.commit()
-            return team_member
 
     def create_team(self, team_data):
         new_team = Team(
