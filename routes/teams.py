c02d5c6952c0be13f010d098f375edcc9327aff7
TomG <tomgosling57@gmail.com>
2025-10-29 15:05:46 +1100

    feat(teams bp):Added url_prefix

--- routes/teams.py
+++ routes/teams.py
@@ -4,7 +4,7 @@ from database import teardown_db
 from flask_login import login_required, current_user
 from controllers import teams_controller
 
-teams_bp = Blueprint('teams', __name__)
+teams_bp = Blueprint('teams', __name__, url_prefix='/teams')
 
 @teams_bp.teardown_request
 def teardown_request(exception=None):
