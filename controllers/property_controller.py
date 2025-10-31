941f0c80c82d8016df9fd971c4c2c75a2f671247
TomG <tomgosling57@gmail.com>
2025-10-31 11:30:06 +1100

    feat(property controller): added property controller stubs and implemented get properties

---
+++ controllers/property_controller.py
@@ -0,0 +1,37 @@
+from flask import jsonify
+from services.property_service import PropertyService
+from database import get_db, teardown_db
+
+def get_properties():
+    """
+    Retrieve all properties.
+    """
+    db = get_db()
+    property_service = PropertyService(db)
+    properties = property_service.get_all_properties()
+    teardown_db()
+    return jsonify({'properties': [prop.__repr__() for prop in properties]}), 200
+
+def get_property_by_id(property_id):
+    """
+    Retrieve a single property by its ID.
+    """
+    pass
+
+def create_property(property_data):
+    """
+    Create a new property.
+    """
+    pass
+
+def update_property(property_id, property_data):
+    """
+    Update an existing property.
+    """
+    pass
+
+def delete_property(property_id):
+    """
+    Delete a property.
+    """
+    pass
