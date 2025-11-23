6bb8f12adb73977e9235793c296b7b59e1845f7e
TomG <tomgosling57@gmail.com>
2025-11-24 09:39:12 +1100

    users controller: refactored update user to use new service methods and helpers

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -4,6 +4,8 @@ from database import get_db, teardown_db
 from services.user_service import UserService
 from flask_login import login_user, current_user, fresh_login_required
 
+from utils.user_helper import UserHelper
+
 def list_all_users_view():
     """List all users with their teams and roles for the owner view."""
     if not current_user.is_authenticated or current_user.role != 'owner':
@@ -101,22 +103,43 @@ def login():
     
     return _return
 
+def get_user_update_form(user_id):
+    """Renders the update form for the given user id."""
+    if not current_user.is_authenticated:
+        return jsonify({'error': 'Unauthorized'}), 403
+    
+    db = get_db()
+    user_service = UserService(db)
+    user = user_service.get_user_by_id(user_id)
+    roles = user_service.get_roles()
+    return render_template('user_update_form.html', user=user, roles=roles)
+
 def update_user(user_id):
-    """Update user information"""
-    data = request.get_json()
+    """Update user details in the database."""
+    if not current_user.is_authenticated:
+        return jsonify({'error': 'Unauthorized'}), 403
+    
+    data = request.form.to_dict()
     if not data:
         return jsonify({'error': 'Invalid data provided'}), 400
 
-    allowed_fields = ['username', 'password', 'role']
-    if not any(field in data for field in allowed_fields):
-        return jsonify({'error': 'No valid fields provided for update'}), 400
-
+    # Clean the user form data and handle errors
     db = get_db()
     user_service = UserService(db)
+    user = user_service.get_user_by_id(user_id)
+    user_helper = UserHelper(db)
+    data = user_helper.clean_user_form_data(data)
+    errors = user_helper.validate_user_form_data(data)
+    # Render errors to the UI
+    if errors:
+        return render_template('user_update_form.html', user=user, errors=errors)
+    
+    # Update the database if there are no errors
     user = user_service.update_user(user_id, data)
-    teardown_db()
     if user:
-        return redirect(url_for('user.list_all_users_view'), code=303)
+        users = user_service.get_all_users()
+        teardown_db()
+        return render_template('user_list_fragment', users=users)
     else:
         return jsonify({'error': 'User not found'}), 404
 
