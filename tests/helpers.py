3d1717693b2caf021fdc5e2f4ce996ce4cbbd68f
TomG <tomgosling57@gmail.com>
2025-11-27 13:52:46 +1100

    test helpers: added login cleaner method and used new passwords

--- tests/helpers.py
+++ tests/helpers.py
@@ -14,5 +14,41 @@ def login_owner(page, goto) -> None:
     page.get_by_role("textbox", name="email").click()
     page.get_by_role("textbox", name="email").fill("owner@example.com")
     page.get_by_role("textbox", name="password").click()
-    page.get_by_role("textbox", name="password").fill("ownerpassword")
+    page.get_by_role("textbox", name="password").fill("owner_password")
+    page.get_by_role("button", name="Login").click()
+
+def login_team_leader(page, goto) -> None:
+    """
+    Executes the login flow for the team leader.
+
+    Args:
+        page: The page pytest-playwright fixture representing the current browser page.
+        goto: A fixture to navigate to a specified URL.
+
+    Returns:
+        None
+    """
+    goto("/")                               
+    page.get_by_role("textbox", name="email").click()
+    page.get_by_role("textbox", name="email").fill("team_leader@example.com")
+    page.get_by_role("textbox", name="password").click()
+    page.get_by_role("textbox", name="password").fill("team_leader_password")
+    page.get_by_role("button", name="Login").click()
+
+def login_cleaner(page, goto) -> None:
+    """
+    Executes the login flow for the cleaner.
+
+    Args:
+        page: The page pytest-playwright fixture representing the current browser page.
+        goto: A fixture to navigate to a specified URL.
+    
+    Returns:
+        None
+    """
+    goto("/")                               
+    page.get_by_role("textbox", name="email").click()
+    page.get_by_role("textbox", name="email").fill("cleaner@example.com")
+    page.get_by_role("textbox", name="password").click()
+    page.get_by_role("textbox", name="password").fill("cleaner_password")
     page.get_by_role("button", name="Login").click()
