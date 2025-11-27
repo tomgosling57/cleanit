ab19edf7e93db4b1d0667e1871e71bbb589b8f10
TomG <tomgosling57@gmail.com>
2025-11-27 13:52:22 +1100

    test login: updated to use new helper methods

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,8 @@
 # tests/test_login.py
 from playwright.sync_api import expect
 
+from tests.helpers import login_owner, login_team_leader
+
 def test_login_owner(page, goto) -> None:
     """
     Tests the login functionality for the owner.
@@ -12,12 +14,7 @@ def test_login_owner(page, goto) -> None:
     Returns:
         None
     """
-    goto("/")                               
-    page.get_by_role("textbox", name="email").click()
-    page.get_by_role("textbox", name="email").fill("owner@example.com")
-    page.get_by_role("textbox", name="password").click()
-    page.get_by_role("textbox", name="password").fill("ownerpassword")
-    page.get_by_role("button", name="Login").click()
+    login_owner(page, goto)
     expect(page).to_have_title("Timetable") # Assert login was successful
     expect(page.get_by_text("Create Job")).to_be_visible() # Assert owner-specific element is visible
 
@@ -32,11 +29,6 @@ def test_login_team_leader(page, goto) -> None:
     Returns:
         None
     """
-    goto("/")                               
-    page.get_by_role("textbox", name="email").click()
-    page.get_by_role("textbox", name="email").fill("team_leader@example.com")
-    page.get_by_role("textbox", name="password").click()
-    page.get_by_role("textbox", name="password").fill("team_leader_password")
-    page.get_by_role("button", name="Login").click()
+    login_team_leader(page, goto)
     expect(page).to_have_title("Timetable") # Assert login was successful
     expect(page.get_by_text("Create Job")).to_be_hidden() # Assert owner-specific element is hidden
