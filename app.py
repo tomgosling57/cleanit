157c3cdd8187eca3acd980147ff216399253420a
TomG <tomgosling57@gmail.com>
2025-12-03 16:01:10 +1100

    moved populate database call into the create app factory

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask_login import LoginManager
 from app_factory import create_app
-from utils.database import populate_database
 
 if __name__ == '__main__':
     login_manager = LoginManager()
     app = create_app(login_manager=login_manager)
-    populate_database(app.config['SQLALCHEMY_DATABASE_URI'])
     app.run(debug=True)
