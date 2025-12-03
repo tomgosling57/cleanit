b0f30c998121a5221bf6e62236fc5d1045d76ed8
TomG <tomgosling57@gmail.com>
2025-12-03 14:35:28 +1100

    created config and test config classes

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import secrets
+import os
+
 DATETIME_FORMATS = {
     "DATE_FORMAT": "%d-%m-%Y",
     "DATE_FORMAT_FLATPICKR": "d-m-Y",
@@ -9,3 +12,16 @@ DATETIME_FORMATS = {
     "TIME_FORMAT_FLATPICKR": "H:i"
 }
 BACK_TO_BACK_THRESHOLD = 15 # MINUTES
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_bytes(32)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join("instance", "cleanit.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SEED_DATABASE_FOR_TESTING = False
+    INSERT_DUMMY_DATA = False
+
+class TestConfig(Config):
+    TESTING = True
+    SEED_DATABASE_FOR_TESTING = True
+    INSERT_DUMMY_DATA = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
