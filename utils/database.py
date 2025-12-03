315a9eb716207b466a357a8934fb58ada4abcd7f
TomG <tomgosling57@gmail.com>
2025-12-03 14:58:50 +1100

    implemented populate database method to insert data outside of app context

---
+++ utils/database.py
@@ -0,0 +1,21 @@
+import os
+from config import Config
+from database import init_db, seed_database
+
+
+def populate_database(database_uri: str):
+    """This function populates the database with dummy data for testing purposes.
+    
+    Args:
+        database_uri (str): The database URI where the dummy data will be inserted.
+    """
+    
+    if "sqlite:///" in database_uri:
+        database_path = database_uri.replace("sqlite:///", "")
+        database_dir = os.path.dirname(database_path)
+        if not os.path.exists(database_dir):
+            os.makedirs(database_dir)
+
+    Session = init_db(database_uri)
+    seed_database(Session)
+    print("Database populated with dummy data.")
