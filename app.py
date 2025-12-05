0c7e5b7ffc2bfb6d54bb20c2d6eb999f7bff8622
TomG <tomgosling57@gmail.com>
2025-12-06 10:28:47 +1100

    added testing config override to default app

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from app_factory import create_app
 
 if __name__ == '__main__':
     login_manager = LoginManager()
-    app = create_app(login_manager=login_manager)
+    app = create_app(login_manager=login_manager, config_override={'DEBUG': True, 'TESTING': True})
     app.run(debug=True)
