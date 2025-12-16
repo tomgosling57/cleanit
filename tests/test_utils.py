050e7eb41b09291eee2d156cc570ebf63bb63ff1
TomG <tomgosling57@gmail.com>
2025-12-16 14:13:54 +1100

    updated test utils time methods to use date time formats from config

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
+from config import DATETIME_FORMATS
 
 def get_future_date(days: int) -> str:
     future_date = datetime.now() + timedelta(days=days)
-    return future_date.strftime("%Y-%m-%d")
+    return future_date.strftime(DATETIME_FORMATS["DATE_FORMAT"])
 
 def get_future_time(hours: int) -> str:
     future_time = datetime.now() + timedelta(hours=hours)
-    return future_time.strftime("%H:%M")
+    return future_time.strftime(DATETIME_FORMATS["TIME_FORMAT"])
