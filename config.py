e922348dc6b144019cf1668a92261cb57dddb926
TomG <tomgosling57@gmail.com>
2026-01-03 13:35:05 +1100

    added development and production file storage environment variables to config  classes

--- config.py
+++ config.py
@@ -16,6 +16,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_bytes(32)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join("instance", "cleanit.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STORAGE_PROVIDER = os.getenv('STORAGE_PROVIDER', 'local')
+    S3_BUCKET = os.getenv('S3_BUCKET', 'your-bucket-name')
+    AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'your-access-key')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', 'your-secret-key')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', './uploads')
 
 class TestConfig(Config):
     TESTING = True
+    STORAGE_PROVIDER = 'local'
+    UPLOAD_FOLDER = './uploads'
