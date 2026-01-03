76f68c84796d69b04f550119c7ad235581ee7385
TomG <tomgosling57@gmail.com>
2026-01-03 14:10:29 +1100

    implemented get_file_url delete_file and file_exists methods in storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -26,3 +26,51 @@ def upload_flask_file(file_storage_object, container, object_name):
         object_name=object_name
     )
     return uploaded_object
+
+def get_file_url(app, container, object_name):
+    """
+    Generates a URL for the given object.
+
+    :param app: Flask app instance
+    :param container: Libcloud container object
+    :param object_name: Object name
+    :return: URL for the object or None if the object does not exist
+    """
+    storage_provider = app.config.get('STORAGE_PROVIDER')
+    if storage_provider == 'local':
+        return url_for('uploads', filename=object_name)
+    elif storage_provider == 's3':
+        try:
+            obj = container.get_object(object_name)
+            return obj.get_cdn_url()
+        except ObjectDoesNotExistError:
+            return None
+    return None
+
+def delete_file(container, object_name):
+    """
+    Deletes a file from the container.
+
+    :param container: Libcloud container object
+    :param object_name: Object name
+    :return: True if the object was deleted, False if it did not exist
+    """
+    try:
+        container.delete_object(object_name)
+        return True
+    except ObjectDoesNotExistError:
+        return False
+
+def file_exists(container, object_name):
+    """
+    Checks if a file exists in the container.
+
+    :param container: Libcloud container object
+    :param object_name: Object name
+    :return: True if the object exists, False otherwise
+    """
+    try:
+        container.get_object(object_name)
+        return True
+    except ObjectDoesNotExistError:
+        return False
