b93019933664bcf393849a73f28761767277319e
Thomas Gosling <tomgosling57@gmail.com>
2026-01-08 22:16:29 +1100

    refactored property routes to use class controller and use a factory for the servicess

--- routes/properties.py
+++ routes/properties.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request
 from flask_login import login_required
-from controllers import property_controller
-from database import teardown_db
+from controllers.property_controller import PropertyController
+from database import get_db, teardown_db
+from services.property_service import PropertyService
+from services.job_service import JobService
 
 properties_bp = Blueprint('properties', __name__, url_prefix='/address-book')
 
@@ -9,36 +11,53 @@ properties_bp = Blueprint('properties', __name__, url_prefix='/address-book')
 def teardown_property_db(exception=None):
     teardown_db(exception)
 
+def get_property_controller():
+    """Create and return a PropertyController instance with request-level database session."""
+    db_session = get_db()
+    property_service = PropertyService(db_session)
+    job_service = JobService(db_session)
+    controller = PropertyController(
+        property_service=property_service,
+        job_service=job_service
+    )
+    return controller
+
 @properties_bp.route('/', methods=['GET'])
 @login_required
 def properties_collection():
-    return property_controller.get_properties_view()
+    controller = get_property_controller()
+    return controller.get_properties_view()
 
 @properties_bp.route('/property/create', methods=['GET', 'POST'])
 @login_required
 def create_property_route():
+    controller = get_property_controller()
     if request.method == 'POST':
-        return property_controller.create_property()
-    return property_controller.get_property_creation_form()
+        return controller.create_property()
+    return controller.get_property_creation_form()
 
 @properties_bp.route('/property/<int:property_id>/details', methods=['GET'])
 @login_required
 def get_property_details(property_id):
-    return property_controller.get_property_by_id(property_id)
+    controller = get_property_controller()
+    return controller.get_property_by_id(property_id)
 
 @properties_bp.route('/property/<int:property_id>/jobs', methods=['GET'])
 @login_required
 def get_property_jobs_route(property_id):
-    return property_controller.get_property_jobs_modal_content(property_id)
+    controller = get_property_controller()
+    return controller.get_property_jobs_modal_content(property_id)
 
 @properties_bp.route('/property/<int:property_id>/update', methods=['GET', 'PUT'])
 @login_required
 def update_property_route(property_id):
+    controller = get_property_controller()
     if request.method == 'PUT':
-        return property_controller.update_property(property_id)
-    return property_controller.get_property_update_form(property_id)
+        return controller.update_property(property_id)
+    return controller.get_property_update_form(property_id)
 
 @properties_bp.route('/property/<int:property_id>/delete', methods=['DELETE'])
 @login_required
 def delete_property_route(property_id):
-    return property_controller.delete_property(property_id)
+    controller = get_property_controller()
+    return controller.delete_property(property_id)
