64b639ca8d58c8b5fcddba2a286a0cfbdd53e883
TomG <tomgosling57@gmail.com>
2026-01-20 11:53:54 +1100

    updated apt to be available at the module level for gunicorn

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask_login import LoginManager
 from app_factory import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    login_manager = LoginManager()
-    app = create_app(login_manager=login_manager, config_override={'DEBUG': True, 'TESTING': True})
     app.run(debug=True)
