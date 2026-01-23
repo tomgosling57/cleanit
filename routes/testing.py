bc1459dbc8702b68c7e425f81ff51f6400f4097a
Tom Gosling <tomgosling57@gmail.com>
2026-01-23 20:33:37 +1100

    updated testing reseed database route to use the database url environment variable

--- routes/testing.py
+++ routes/testing.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 from utils.populate_database import populate_database
+import os
 testing_bp = Blueprint('testing', __name__, url_prefix='/testing')
 
 
 @testing_bp.route('/reseed-database', methods=['GET'])
 def reseed_database():
     """Deletes all data in the database and reseeds it with deterministic test data"""
-    populate_database(current_app.config['SQLALCHEMY_DATABASE_URI'])
+    populate_database(os.environ['DATABASE_URL'])
     return "Database reseeded", 200
