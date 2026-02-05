c556e2992d2be3ed4c68b4aa65c4e5075b061486
TomG <tomgosling57@gmail.com>
2026-02-05 12:49:03 +1100

    added job service push un complete jobs to next day call to the app.py after the app starts running

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from app_factory import create_app
+from database import get_db
+from services.job_service import JobService
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    # Auto push past uncompleted jobs to current time on startup to prevent stale jobs
+    JobService(get_db()).push_uncompleted_jobs_to_next_day()
+    
